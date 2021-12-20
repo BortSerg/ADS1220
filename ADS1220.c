b116a757feae2412b3ebd76d97a6f6bc0754bc10
Bort Serg <bort.s@ajax.systems>
2021-12-20 14:33:11 +0200

    create repo

---
+++ ADS1220.c
@@ -0,0 +1,79 @@
+#include Arduino.h
+#include ADS1220.h
+#include <SPI.h>
+
+ADS1220 :: ADS1220(){
+
+}
+
+void ADS1220 :: WtiteConfig (uint8_t address, uint8_t value){
+    digitalWrite(default_cs_pin, LOW);
+    SPI.transfer (WREG | (address<<2));
+    SPI.transfer (value);
+    digitalWrite(default_cs_pin, HIGH);
+}
+
+void ADS1220 :: ReadConfig (uint8_t address){
+    uint8_t register_value;
+
+    digitalWrite(default_cs_pin, LOW);
+    SPI.transfer (RREG | (address << 2));
+    register_value = SPI.transfer(SPI_READ);
+    digitalWrite(default_cs_pin, HIGH);
+    Serial.print ("Register " + String(address) + " = " + String (register_value) + "\n\n");
+}
+
+void ADS1220 :: SetDefaultSettings (void){
+    WtiteConfig (config_address_reg0, default_value_reg0);
+    WtiteConfig (config_address_reg1, default_value_reg1);
+    WtiteConfig (config_address_reg2, default_value_reg2);
+    WtiteConfig (config_address_reg3, default_value_reg3);
+}
+
+void ADS1220 :: begin (void){
+    pinMode(default_cs_pin, OUTPUT);
+    pinMode(default_rdy_pin, INPUT);
+
+    SPI.begin();
+    SPI.setBitOrder(MSBFIRST);
+    SPI.setDataMode(SPI_MODE1);
+    SetDefaultSettings();
+}
+
+void ADS1220 :: begin(uint8_t cs_pin, uint8_t rdy_pin){
+    default_cs_pin = cs_pin;
+    default_rdy_pin = rdy_pin;
+
+    pinMode(default_cs_pin, OUTPUT);
+    pinMode(default_rdy_pin, INPUT);
+
+    SPI.begin();
+    SPI.setBitOrder(MSBFIRST);
+    SPI.setDataMode(SPI_MODE1);
+    SetDefaultSettings();
+}
+
+void ADS1220 :: ADS1220_START (void){
+    digitalWrite(default_cs_pin, LOW);
+    SPI.transfer(START);
+    digitalWrite(default_cs_pin, HIGH);
+}
+
+void ADS1220 :: ADS1220_RESET (void){
+    digitalWrite(default_cs_pin, LOW);
+    SPI.transfer(RESET); 
+    digitalWrite(default_cs_pin, HIGH);
+}
+
+
+
+void ADS1220 :: SetSettings (uint8_t address, uint8_t value){
+
+}
+
+
+
+
+
+
+
---
+++ ADS1220.h
@@ -0,0 +1,35 @@
+#include Arduino.h
+#include "SPI.h"
+
+#define SPI_READ   0xFF
+#define RESET      0x06   
+#define START      0x08    
+#define WREG       0x40
+#define RREG       0x20
+
+#define config_address_reg0 0x00
+#define config_address_reg1 0x01
+#define config_address_reg2 0x02
+#define config_address_reg3 0x03
+
+#define default_value_reg0 0x00
+#define default_value_reg1 0x04
+#define default_value_reg2 0x10
+#define default_value_reg3 0x00
+
+class ADS1220
+{
+private:
+        uint8_t default_cs_pin  = 7;
+        uint8_t default_rdy_pin = 6;
+public:
+
+        ADS1220();
+        void begin (void);
+        void begin (uint8_t cs_pin, uint8_t rdy_pin);
+        void ReadConfig (uint8_t address);
+        void SetDefaultSettings (void);
+        void ADS1220_START (void);
+        void ADS1220_RESET (void);
+
+}
