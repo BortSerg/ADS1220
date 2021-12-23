91570803f2cdbf755116980e2e8c0ea22461d3dd
Bort Sergey <bort.sergei@gmail.com>
2021-12-23 22:43:57 +0200

    add examples code and keywords list

---
+++ examples/examples.ino
@@ -0,0 +1,24 @@
+#include "ADS1220.h"
+
+#define cs_pin 7
+#define rdy_pin 8
+
+ADS1220 ADC;
+
+void setup()
+{
+    ADC.begin(cs_pin, rdy_pin);
+    ADC.PGA(PGA_BYPASS_ON);
+    ADC.Gain(GAIN_1);
+    ADC.MuxChanel(MUX_AIN0_AIN1);
+    ADC.TemperatureSensor(TS_ON);
+    ADC.ConversionMode(CONTINUOUS_MODE);
+    ADC.OperatingMode(DUTY_CYCLE_MODE);
+    ADC.DataRate(DR_20SPS);
+    ADC.FIR(FIR_50);
+}
+
+void loop()
+{
+
+}
